5699b78e53729025b112547a5f3765b20c7b99f6
vincentNtabaazi <vincentntabaazi02@gmail.com>
2024-08-09 18:42:15 +0300

    The user is able to place an order and receive a success message after the order has been placed successfully.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Item;
 
 class Product extends Model
 {
@@ -13,7 +14,24 @@ class Product extends Model
      * $this->attributes['description'] - string - contains the product description
      * $this->attributes['image'] - string - contains the product image
      * $this->attributes['price'] - int - contains the product price
-     * $this->attributes['created_at'] - timestamp - contains the product creation date * $this->attributes['updated_at'] - timestamp - contains the product update date */
+     * $this->attributes['created_at'] - timestamp - contains the product creation date * $this->attributes['updated_at'] - timestamp - contains the product update date
+     * $this->items - Item[] - contains the associated items 
+     * */
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+
+    public function getItems()
+    {
+        return $this->items;
+    }
+    
+    public function setItems($items)
+    {
+        $this->items = $items;
+    }
 
     public static function validate($request)
     {
